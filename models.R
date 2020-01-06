88afd4ce5566fdcfaab9c144a130cf5f7d38e46a
Jannis <jannis-kambach@gmx.de>
2020-01-07 00:05:01 +0100

    ToDos

--- models.R
+++ models.R
@@ -6,8 +6,7 @@ library(DMwR)
 #------------------------------------
 
 d18_learning = d18_imputed[,-c(1,14)]
-d18_learning$Flag_cancellation = as.factor(d18_learning$Flag_cancellation)
-d18_balanced <- SMOTE(Flag_cancellation ~ ., d18_learning, perc.over=300,perc.under=1000)
+d18_balanced <- SMOTE(Flag_cancellation ~ ., d18_learning, perc.over=200,perc.under=300)
 
 #------------------------
 # Create Task and Learner
@@ -29,4 +28,12 @@ rinst_SMOTE = makeResampleInstance(rdesc,task_SMOTE)
 # Train model
 #------------
 
-res = resample(learner,task,rinst,measures=list(mmce,tpr,tnr))
+res = resample(learner,task_SMOTE,rinst_SMOTE,measures=list(mmce,tpr,tnr),models=T)
+res$models[[1]]$learner.model
+
+### TODO
+# 1: train one model on entire dataset
+# 2: add decision tree (incl. hyperparameter tuning)
+# 3: maybe SMOTE optimization? -> needs to be after splitting training and test data
+# 4: visualization (https://christophm.github.io/interpretable-ml-book/logistic.html)
+# 5: prediction
